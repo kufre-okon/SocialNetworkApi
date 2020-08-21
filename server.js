2e915e7e76c9f7e96015fe3c5bd300588d22c81b
sagamydev1 <kufreokon24@gmail.com>
2020-08-21 11:47:37 +0100

    Remove cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const config = require('./config/config');
 const ApiResponse = require('./helpers/apiresponse.helper');
@@ -10,14 +10,19 @@ const app = express();
 
 require('./swagger_init')(app);
 
-var corsOptions = {
-    origin: ['*']
-}
-
-app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(morgan('dev'));
+/*
+I was getting "Access to XMLHttpRequest at '' from origin '' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource."
+when I used Cors above especially when posting FormData
+*/
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 mongoose.connect(config.DB_CONNECTION_STRING, {
     useNewUrlParser: true,
