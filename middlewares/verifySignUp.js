6148949918983de68a4c804df695fb70e47d2325
sagamydev1 <kufreokon24@gmail.com>
2020-10-31 22:33:36 +0100

    - Fixed vulnerability issue

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -1,5 +1,5 @@
 
-//const ROLES = db.ROLES;
+const ROLES = [];
 
 const User = require('../models/user.model');
 const ApiResponse = require('../helpers/apiresponse.helper');
@@ -30,10 +30,11 @@ const checkDuplicateUsernameOrEmail = async (req, res, next) => {
 
 const checkRolesExisted = (req, res, next) => {
     if (req.body.roles) {
-        for (let i = 0; i < req.body.roles.length; i++) {
-            if (!ROLES.includes(req.body.roles[i])) {
+        const roles = req.body.roles;
+        for (let i = 0; i < roles.length; i++) {
+            if (!ROLES.includes(roles[i])) {
                 res.status(400).send({
-                    message: `Failed! Role ${req.body.roles[i]} does not exist!`
+                    message: `Failed! Role ${roles[i]} does not exist!`
                 })
                 return;
             }
